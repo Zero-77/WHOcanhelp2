8bbbcf222724d10e90b620a23a98e1f39c362e20
Zero-77 <07072u4bp6@gmail.com>
2020-04-20 23:48:18 +0800

    Initial Commit

---
+++ WHOcanhelp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WHOcanhelp
+//
+//  Created by 07 Zhang on 2020/4/20.
+//  Copyright © 2020 07 Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
